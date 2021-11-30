a57eb684d1c5c0b0a7d3a7faf00972c9f33ca550
Yoni Lerner <yonilern@gmail.com>
2021-11-30 01:36:16 -0800

    Works only for unique_leaves

--- explore_data.py
+++ explore_data.py
@@ -7,6 +7,25 @@ from tqdm import tqdm
 import itertools
 
 
+# For nice printing
+def p(d):
+	print(json.dumps(d, indent=2))
+
+# For searching a nested dict for stuff
+def search(d, s, caseSensitive=True):
+	if type(d) is dict:
+		return any(search(d2, s) for d2 in d.values()) or any(search(d2, s) for d2 in d.keys())
+	elif type(d) is list or type(d) is set:
+		return any(search(d2, s) for d2 in d)
+	elif type(d) is str:
+		if not caseSensitive:
+			d = d.lower()
+		return s in d
+	else:
+		return d == s
+
+
+
 if __name__ == "__main__":
 	with open(sys.argv[1]) as f:
 		d = json.load(f)
@@ -41,60 +60,99 @@ if __name__ == "__main__":
 			i += 1
 		return d
 
-	print("Getting all unique leaves...")
-	unique_leaves = {}
-	unique_leaves_lookup = {}
-	unique_leaves_counts = {}
-	for keylist in tqdm(leaf_keys):
-		# Skip sources because dicts in lists is a headache
-		if keylist[0] == "sources":
-			continue
+	# def process_unique_leaves_recursively(d, unique_leaves, unique_leaves_lookup, unique_leaves_counts):
+	# 	if type(d) is dict:
+	# 		for k, v in d.items():
+	# 			for subd in [unique_leaves, unique_leaves_lookup, unique_leaves_counts]:
+	# 				if not k in subd:
+	# 					subd[k] = {}
 
-		subd = unique_leaves
-		for subd in [unique_leaves, unique_leaves_lookup, unique_leaves_counts]:
-			for key in keylist[:-1]:
-				if not key in subd:
-					subd[key] = {}
-				subd = subd[key]
-			
-		get_from_dict(unique_leaves, keylist[:-1])[keylist[-1]] = set()
-		get_from_dict(unique_leaves_lookup, keylist[:-1])[keylist[-1]] = {}
-		get_from_dict(unique_leaves_counts, keylist[:-1])[keylist[-1]] = {}
-		
-		for k, v in d.items():
-			val = get_from_dict(v, keylist)
-			if val is not False:
-				if type(val) is list and any(isinstance(x, list) or isinstance(x, set) for x in val): # Flatten lists of lists (e.g. attacks_melee)
-					val = set(itertools.chain.from_iterable(val))
-				elif type(val) is not set and type(val) is not list:
-					val = set([val])
+	# 			o = process_unique_leaves_recursively(v, unique_leaves[k], unique_leaves_lookup[k], unique_leaves_counts[k])
+	# 			if type(o) is set: # It's a leaf
+	# 				if not type(unique_leaves[k]) is set:
+	# 					assert len(unique_leaves[k]) == 0
+	# 					unique_leaves[k] = set()
+	# 				unique_leaves[k].update(o)
+	# 			else:
+	# 				assert type(o) is dict
+	# 	elif type(d) is list or type(d) is set:
 
-				get_from_dict(unique_leaves, keylist).update(val)
+	# 		for v in d.items():
+	# 			o = process_unique_leaves_recursively(v, unique_leaves, unique_leaves_lookup, unique_leaves_counts)
+	# 			if type(o) is set: # It's a leaf
 
-				for subval in val:
-					d_lookup = get_from_dict(unique_leaves_lookup, keylist)
-					if not subval in d_lookup:
-						d_lookup[subval] = []
-					d_lookup[subval].append(k)
+	def join_nested_dicts_of_sets(d1, d2): # Inserts d2 into d1
+		if type(d1) is set:
+			assert type(d2) is set
+			d1.update(d2)
+			return
 
-					d_counts = get_from_dict(unique_leaves_counts, keylist)
-					if not subval in d_counts:
-						d_counts[subval] = 0
-					d_counts[subval] += 1
+		for k, v in d2.items():
+			if not k in d1:
+				d1[k] = v
+			else:
+				join_nested_dicts_of_sets(d1[k], v)
 
-# For nice printing
-def p(d):
-	print(json.dumps(d, indent=2))
+	# d1 = {
+	# 	"a": {
+	# 		"X": {
+	# 			"o1": set([1, 2, 3]),
+	# 			"o2": set([3, 5])
+	# 		},
+	# 		"Y": set([99])
+	# 	},
+	# 	"b": set([10, 5])
+	# }
+	# d2 = {
+	# 	"a": {
+	# 		"X": {
+	# 			"o1": set([3, 91, 92]),
+	# 			"o3": set([9, 100])
+	# 		},
+	# 		"Z": {
+	# 			"k1": set([3]),
+	# 			"k2": set([-1, -2])
+	# 		}
+	# 	},
+	# 	"b": set([11, 55]),
+	# 	"c": set([88])
+	# }
 
-# For searching a nested dict for stuff
-def search(d, s, caseSensitive=True):
-	if type(d) is dict:
-		return any(search(d2, s) for d2 in d.values()) or any(search(d2, s) for d2 in d.keys())
-	elif type(d) is list or type(d) is set:
-		return any(search(d2, s) for d2 in d)
-	elif type(d) is str:
-		if not caseSensitive:
-			d = d.lower()
-		return s in d
-	else:
-		return d == s
+	def semi_flatten_nested_data(d):
+		if type(d) is dict:
+			return {k: semi_flatten_nested_data(v) for k, v in d.items()}
+		elif type(d) is list or type(d) is set:
+			r = [semi_flatten_nested_data(x) for x in d]
+			r0 = r[0]
+			for ri in r[1:]:
+				join_nested_dicts_of_sets(r0, ri)
+			return r0
+		else:
+			return set([d])
+
+	# testd = [
+	# 	{
+	# 		"a": [
+	# 			{
+	# 				"X": [1, 2, 3]
+	# 			},
+	# 			{
+	# 				"Y": [4, 5, 6]
+	# 			}
+	# 		],
+	# 		"b": 10,
+	# 	},
+	# 	{
+	# 		"a": [
+	# 			{
+	# 				"X": [2, 5]
+	# 			},
+	# 			{
+	# 				"Z": [3, 4]
+	# 			}
+	# 		],
+	# 		"b": 5,
+	# 	}
+	# ]
+
+	unique_leaves = semi_flatten_nested_data(list(d.values()))
